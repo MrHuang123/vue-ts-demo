9112bad234b404e8fbfc937740c0070a965326f5
huangtianchang <charlie.huang@transn.com>
2020-07-16 17:53:26 +0800

    @huangtianchang:提交

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,28 @@
 import {Common} from '@/interface/common';
-import {Store} from '@/interface/store'
+import {Store} from '@/interface/store';
+
+const STATE:Common = {
+  height: window.innerHeight,
+  width: window.innerWidth,
+}
+const GETTERS:Common = {
+
+}
+const MUTATIONS:Common = {
+  changeKey(state: Store.State, payload: Common) {
+    for (const i in payload) {
+      state[i] = null;
+      state[i] = payload[i];
+    }
+  },
+}
+const ACTIONS:Common = {
+
+}
 export default {
-  state: {},
-  mutations: {},
-  actions: {},
+  state: STATE,
+  getters:GETTERS,
+  mutations: MUTATIONS,
+  actions: ACTIONS,
   modules: {}
 };
